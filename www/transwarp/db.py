92dcd38a10f08c750f096c277ea06872940cb534
Jon <wayupto@sina.com>
2015-05-14 01:26:49 +0800

    finish day02

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -1,6 +1,5 @@
-# db.py
 #!/usr/bin/env python
-# -*-coding:utf8-*-
+# -*- coding:utf-8 -*-
 
 import time, uuid, threading, logging
 
@@ -10,11 +9,72 @@ class _Engine(object):
 		self._connect = connect
 
 	def connect(self):
-		return self._connect
+		return self._connect()  #因为传入的是lambda表达式，所以是个函数，要加括号
 
+class DBError(Exception):
+	pass
+
+class MultiColumnsError(DBError):
+	pass
+
+class Dict(dict):
+	'''
+	能支持 x.y 形式的dict,不需要每次都写x[y]
+	'''
+	def __init__(self, names=(), values=(), **kw):
+		super(Dict, self).__init__(**kw)
+		for k, v in zip(names, values):
+			self[k] = v
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
+
+
+
+def next_id(t=None):
+	'''
+	return next id as 50-char string.
+
+	参数:
+		t 默认是none，用的是时间戳 time.time()
+	'''
+	if t is None:
+		t = time.time()
+	return '%015d%s000' % (int(t * 1000), uuid.uuid4().hex)
+
+def _profiling(start, sql=''):
+	t = time.time() - start
+	if t > 0.1:
+		logging.warning('[PROFILING] [DB] %s: %s' % (t, sql))
+	else:
+		logging.info('[PROFIlING] [DB] %s: %s' % (t, sql))
+
+# global engine object
 engine = None
 
-class _lazyConnection(object):
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+	import mysql.connector
+	global engine
+	if engine is not None:
+		raise DBError('Engine is already initialized...')
+	params = dict(user=user, password=password, database=database, host=host, port=port)
+	defaults = dict(use_unicode=True, charset='utf8', collation='utf8_general_ci',autocommit=False)
+	for k,v in defaults.iteritems():
+		params[k] = kw.pop(k, v)
+	params.update(kw)
+	params['buffered'] = True
+	engine = _Engine(lambda: mysql.connector.connect(**params))
+	# test connection...
+	logging.info('Init mysql engine <%s> complete...' % hex(id(engine)))
+
+
+class _LazyConnection(object):
 	def __init__(self):
 		self.connection = None
 
@@ -22,7 +82,6 @@ class _lazyConnection(object):
 		if self.connection is None:
 			self.connection = engine.connect()
 			logging.info('open connection %s...' % hex(id(connection)))
-			self.connection = connection
 		return self.connection.cursor()
 
 	def commit(self):
@@ -35,6 +94,8 @@ class _lazyConnection(object):
 		if self.connection:
 			self.connection.close()
 			self.connection = None
+			logging.info('close connection %s...' % hex(id(connection)))
+
 
 # 持有数据库连接的上下文对象
 class _DbCtx(threading.local):
@@ -62,7 +123,7 @@ class _ConnectionCtx(object):
 	def __enter__(self):
 		global _db_ctx
 		self.should_cleanup = False	
-		if not _db_ctx.is_init:
+		if not _db_ctx.is_init():
 			_db_ctx.init()
 			self.should_cleanup = True
 		return self
@@ -80,16 +141,8 @@ import functools
 def with_connection(func):
 	@functools.wraps(func)
 	def wrapper(*args, **kw):
-		global _db_ctx
-		should_cleanup = False	
-		if not _db_ctx.is_init:
-			_db_ctx.init()
-			should_cleanup = True
-
-		func(*args, **kw)
-
-		if should_cleanup:
-			_db_ctx.cleanup()
+		with _ConnectionCtx():
+			return func(*args, **kw)
 	return wrapper
 
 
@@ -97,7 +150,7 @@ class _TransactionCtx(object):
 	def __enter__(self):
 		global _db_ctx
 		self.should_close_conn = False
-		if not _db_ctx.is_init:
+		if not _db_ctx.is_init():
 			_db_ctx.init
 			self.should_close_conn = True
 			_db_ctx.transactions += 1
@@ -126,3 +179,99 @@ class _TransactionCtx(object):
 	def rollback(self):
 		global _db_ctx
 		_db_ctx.connection.rollback()
+
+def transaction():
+	return _TransactionCtx()
+
+def with_transaction(func):
+	@functools.wraps(func)
+	def wrapper(*args, **kw):
+		_start = time.time()
+		with _TransactionCtx():
+			return func(*args, **kw)
+		_profiling(_start)
+	return wrapper
+
+def _select(sql, first, *args):
+	'执行查询语句，并返回查询结果 或者是 list形式的结果'
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?', '%s')
+	logging.info('SQL:%s, ARGS:%s' % (sql, args))
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql, args)
+		if cursor.description:
+			names = [x[0] for x in cursor.description]
+		if first:
+			values = cursor.fetchone()
+			if not values:
+				return None
+			return Dict(names, values)
+		return [Dict(names, x) for x in cursor.fetchall()]
+	finally:
+		if cursor:
+			cursor.close()
+
+@with_connection
+def select_one(sql, *args):
+	'''
+	查询一条结果
+	'''
+	return _select(sql, True, *args)
+
+
+@with_connection
+def select_int(sql, *args):
+	'''
+	查询一个类型为int的结果
+	'''
+	d = _select(sql, True, *args)
+	if len(d) != 1:
+		raise MultiColumnsError('Expect only one column.')
+	return d.values()[0]
+
+
+@with_connection
+def select(sql, *args):
+	return _select(sql, False, *args)
+
+
+@with_connection
+def _update(sql, *args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?', '%s')
+	logging.info('SQL: %s, ARGS: %s' %(sql, args))
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql, args)
+		r = cursor.rowcount
+		if _db_ctx.transactions==0:
+			logging.info('auto commit')
+			_db_ctx.connection.commit()
+		return r
+	finally:
+		if cursor:
+			cursor.close()		
+
+def insert(table, **kw):
+	cols, args = zip(**kw.iteritems())
+	sql = 'insert into "%s" (%s) values (%s)' % (table, ','.join(['"%s"' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+	return _update(sql, *args)
+
+
+def update(sql, *args):
+	return _update(sql, *args)
+
+if __name__=='__main__':
+	logging.basicConfig(level=logging.DEBUG)
+	create_engine('root', 'abcd', 'test')
+	update('drop table if exists user')
+	update('create table user (id int primary key, name text, email text, passwd text, last_modified real)')
+	import doctest
+	doctest.testmod()
+
+
+
+
